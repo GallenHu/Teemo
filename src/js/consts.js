cd5d12f6673fc24df67b792687a843a23d6ec0d3
GallenHu <gallenhu@foxmail.com>
2019-12-20 09:49:15 +0800

    refactor: default icon

--- src/js/consts.js
+++ src/js/consts.js
@@ -1,8 +1,6 @@
-const heartIcon = require('../asserts/svg/heart.svg').default;
-
 export default {
   CLASS_NAME_OF_ENGINES: 'engine-logo',
   ADD_SITE_TEXT: '添加网址',
-  DEFAULT_ICON: heartIcon,
+  DEFAULT_ICON: 'https://img.hinpc.com/img/picgo/20191220094350_heart.svg',
   STORAGE_NAME: 'nav_sites',
 };

4bbf9c2ed8368dad8af3a8c516e0d9213837a51a
GallenHu <bear@fish.cat>
2020-04-13 14:49:45 +0800

    feat: search section

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,6 +2,9 @@ $(document).ready(() => {
   $('.engine-choose').click(function () {
     onClickEngine($(this).attr('class').replace('engine-choose ', '').toLowerCase());
   });
+  $('.nav-toggle').click(onClickNavToggle);
+  $('.search-form').on('submit', onSearchFormSubmit);
+  initBaiduSug();
 
   const lastEngine = getLastEngine() || '';
   if (lastEngine) {
@@ -11,6 +14,8 @@ $(document).ready(() => {
 
 function onClickEngine(engine) {
   $('.searcher-logo').attr('class', 'searcher-logo').addClass(engine);
+  $('.engine-choose').removeClass('current');
+  $(`.engine-choose.${engine}`).addClass('current');
   $('#searchInputEl').attr('placeholder', `${firstUpperCase(engine)} 搜索`);
   storeLastEngine(engine);
 }
@@ -25,4 +30,38 @@ function storeLastEngine(engine) {
 
 function getLastEngine() {
   return localStorage.getItem('engine');
+}
+
+function onClickNavToggle() {
+  $('.main-inner').toggleClass('is-nav-shrink');
+}
+
+function onSearchFormSubmit(e) {
+  e.preventDefault();
+  const text = $('#searchInputEl').val();
+  const engine = $('.searcher-logo').attr('class').replace('searcher-logo ', '');
+  goSearch(engine, text);
+}
+
+function initBaiduSug() {
+  new BaiduSug('searchInputEl', {
+    className: 'hinote-search',
+    border: '1px solid #ddd',
+    xOffset: -1,
+    maxCount: 9,
+    callback: function(text) {
+      $('#searchInputEl').val(text);
+      $('#searchSubmitEl').trigger('click');
+    }
+  });
+}
+
+function goSearch(engine, text) {
+  const SearchEngineUrlMap = {
+    baidu: 'https://www.baidu.com/s?wd=',
+    google: 'https://www.google.com/search?q=',
+    dogedoge: 'https://www.dogedoge.com/results?q='
+  };
+
+  window.open(SearchEngineUrlMap[engine] + text);
 }

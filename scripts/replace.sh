4fa4e93a2c2c950d1805e25114ae39c6183f56c6
Gallen.Hu <gallenhu@foxmail.com>
2020-05-11 21:44:31 +0800

    Update replace.sh

--- scripts/replace.sh
+++ scripts/replace.sh
@@ -2,9 +2,9 @@ cp template/script-with-vars.php dist
 cd dist
 
 # delete line 11~24
-sed -i '' '11,24d' index.html
+sed -i '11,24d' index.html
 
 # append content to line 10
-sed -i '' '10 r script-with-vars.php' index.html
+sed -i '10 r script-with-vars.php' index.html
 
 cp index.html index.php

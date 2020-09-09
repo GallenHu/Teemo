ab9937cc5fa85ce67ecf946ed4a8b3ef2dd37e61
Gallen.Hu <gallenhu@foxmail.com>
2020-09-09 09:49:40 +0800

    Update main.js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -90,7 +90,7 @@ function initBaiduSug() {
 
 function goSearch(engine, text) {
   const SearchEngineUrlMap = {
-    baidu: 'http://img.hinpc.com/allso/#',
+    baidu: 'https://img.hinpc.com/allso/#',
     google: 'https://www.google.com/search?q=',
     dogedoge: 'https://www.dogedoge.com/results?q=',
   };

da1e6d1a184d6f29880673f8d5138485b72bc0be
Gallen.Hu <gallenhu@outlook.com>
2021-04-09 15:48:56 +0800

    chore: debug travis

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -6,7 +6,6 @@ repo_name2=`echo ${name1}| tr '[:upper:]' '[:lower:]'`
 mkdir -p /tmp/my-travis-build/${repo_name2}
 cp -rf ./build/ /tmp/my-travis-build/${repo_name2}
 
-echo ${repo_name2}
 cd /tmp/my-travis-build/
-ls
-sshpass -p "${SERVER_PWD}" scp -P 1991 -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -r ${repo_name2} root@qhk.nicebook.win:/home/wwwroot/
+# sshpass -p "${SERVER_PWD}" scp -P 1991 -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -r ${repo_name2} root@qhk.nicebook.win:/home/wwwroot/
+scp -P 1991 -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -r ${repo_name2} root@qhk.nicebook.win:/home/wwwroot/

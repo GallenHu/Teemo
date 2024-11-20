980b6b83f2869d0edfb0256610f6ed128f147deb
Gallen.Hu <gallenmbp@gmail.com>
2024-11-20 18:29:51 +0800

    feat: auto-complete

--- components/section-landing/search-box.tsx
+++ components/section-landing/search-box.tsx
@@ -1,37 +1,71 @@
 "use client";
-import { autocomplete } from "@algolia/autocomplete-js";
 import "@algolia/autocomplete-theme-classic";
-import { useEffect } from "react";
+import React from "react";
+import { Autocomplete } from "./auto-complete";
 
 export function SearchBox() {
-  const items = [{ value: "Apple" }, { value: "Banana" }];
+  const debounced = debouncePromise(
+    (items: any[]) => Promise.resolve(items),
+    300
+  );
 
-  useEffect(() => {
-    autocomplete({
-      container: "#autocomplete",
-      placeholder: "Search for products",
-      getSources({ query }) {
-        return fetch(`/api/search/complete?query=${encodeURIComponent(query)}`)
-          .then((response) => response.json())
-          .then((data) => {
-            console.log(data);
+  function getSources({ query }) {
+    return debounced([
+      {
+        sourceId: "suggestions",
+        getItems() {
+          if (!query) {
+            return [];
+          }
 
-            return [
-              {
-                sourceId: "suggestions",
-                getItems() {
-                  return data.suggestions;
-                },
-                getItemInputValue({ item }) {
-                  return item.value;
-                },
-                // ...
-              },
-            ] as any;
-          });
+          return fetch(
+            `/api/search/complete?query=${encodeURIComponent(query)}`
+          )
+            .then((response) => response.json())
+            .then((data) => {
+              const suggestions = data.suggestions || [];
+              console.log(suggestions);
+              return suggestions.map((s) => {
+                return {
+                  label: s.value,
+                  url: s.serpapi_link,
+                };
+              });
+            });
+        },
+        getItemUrl({ item }) {
+          return item.url;
+        },
+        templates: {
+          item({ item, components }) {
+            return (
+              <a href={item.url} className="aa-ItemLink">
+                <div className="aa-ItemContent">
+                  <div className="aa-ItemTitle">
+                    <components.Highlight hit={item} attribute="label" />
+                  </div>
+                </div>
+              </a>
+            );
+          },
+        },
       },
-    });
-  }, []);
+    ]);
+  }
 
-  return <div id="autocomplete"></div>;
+  return <Autocomplete openOnFocus={true} getSources={getSources} />;
+}
+
+function debouncePromise(fn: any, time: number) {
+  let timer: any = undefined;
+
+  return function debounced(...args: any[]) {
+    if (timer) {
+      clearTimeout(timer); // Clear the timeout first if it's already defined.
+    }
+
+    return new Promise((resolve) => {
+      timer = setTimeout(() => resolve(fn(...args)), time);
+    });
+  };
 }

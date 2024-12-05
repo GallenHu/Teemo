ee4f4778f9825f0f899b0c3097ee4b2eb70fe953
Gallen.Hu <gallenmbp@gmail.com>
2024-12-05 11:39:43 +0800

    build: add docker build

--- auth.ts
+++ auth.ts
@@ -1,6 +1,4 @@
 import NextAuth from "next-auth";
-import GitHub from "next-auth/providers/github";
-import Gitlab from "next-auth/providers/gitlab";
 import { MongoDBAdapter } from "@auth/mongodb-adapter";
 import client from "./lib/db";
 import authConfig from "./auth.config";
